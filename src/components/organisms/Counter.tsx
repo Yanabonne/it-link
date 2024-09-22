4b8d35b210e8240886fee90e8f0bf8983f0415ce
iana <yanamormmm@gmail.com>
2024-09-22 18:55:46 +0500

    feat: update debounce flow with useMemo and add useCallback

--- src/components/organisms/Counter.tsx
+++ src/components/organisms/Counter.tsx
@@ -3,7 +3,8 @@ import React, {
   ReactNode,
   useContext,
   useState,
-  useEffect,
+  useMemo,
+  useCallback,
 } from "react";
 import useDebounce from "@/utils/debounce";
 
@@ -20,21 +21,20 @@ type CounterProviderProps = {
 const CounterContext = createContext<CounterContextType | undefined>(undefined);
 
 function CounterProvider({ children }: CounterProviderProps) {
-  const [count, setCount] = useState(0);
   const [countInternal, setCountInternal] = useState(0);
 
   const debouncedCountValue = useDebounce(countInternal, 500);
-
-  useEffect(() => {
-    if (debouncedCountValue) {
-      setCount(debouncedCountValue);
-    } else {
-      setCount(0);
-    }
+  const count = useMemo(() => {
+    return debouncedCountValue;
   }, [debouncedCountValue]);
 
-  const increment = () => setCountInternal((prevCount) => prevCount + 1);
-  const decrement = () => setCountInternal((prevCount) => prevCount - 1);
+  const increment = useCallback(() => {
+    setCountInternal((prevCount) => prevCount + 1);
+  }, []);
+
+  const decrement = useCallback(() => {
+    setCountInternal((prevCount) => prevCount - 1);
+  }, []);
 
   return (
     <CounterContext.Provider value={{ count, increment, decrement }}>
@@ -46,7 +46,7 @@ function CounterProvider({ children }: CounterProviderProps) {
 function useCounterContext() {
   const context = useContext(CounterContext);
   if (!context) {
-    throw new Error("useCounter must be used within the Counter Provider");
+    throw new Error("App context must be used within the provider");
   }
   return context;
 }
